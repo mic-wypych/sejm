e13ee0cafb49d58808a9875fc1d9c2c3e351eecf
MWypych <wypych.mic@gmail.com>
2025-06-10 12:58:15 +0200

    add get_committees

---
+++ R/get_committees.R
@@ -0,0 +1,12 @@
+#get committees, includes members
+
+get_comittees <- function(term= 10) {
+  req_url <- paste0("https://api.sejm.gov.pl/sejm/term", term, "/")
+  req <- request(req_url)
+
+  req |>
+    req_url_path_append("committees") |>
+    req_user_agent("sejm (https://github.com/mic-wypych/sejm)") |>
+    req_perform() |>
+    resp_body_json(simplifyVector = TRUE)
+}
