f7060ecf5ad4a72ada4126504c86919ae29ba196
MWypych <wypych.mic@gmail.com>
2025-06-10 12:57:39 +0200

    add get_questions

---
+++ R/get_questions.R
@@ -0,0 +1,12 @@
+#get written questions
+
+get_questions <- function(term= 10) {
+  req_url <- paste0("https://api.sejm.gov.pl/sejm/term", term, "/")
+  req <- request(req_url)
+
+  req |>
+    req_url_path_append("writtenQuestions") |>
+    req_user_agent("sejm (https://github.com/mic-wypych/sejm)") |>
+    req_perform() |>
+    resp_body_json(simplifyVector = TRUE)
+}
