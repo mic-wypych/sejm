ed7cebb80d891c7a706b4d7bb64106da6b3cce1a
MWypych <wypych.mic@gmail.com>
2025-06-10 12:57:09 +0200

    add geT_interpellations

---
+++ R/get_interpellations.R
@@ -0,0 +1,12 @@
+# get interpellations
+
+get_interpellations <- function(term = 10) {
+  req_url <- paste0("https://api.sejm.gov.pl/sejm/term", term, "/")
+  req <- request(req_url)
+
+  req |>
+    req_url_path_append("interpellations") |>
+    req_user_agent("sejm (https://github.com/mic-wypych/sejm)") |>
+    req_perform() |>
+    resp_body_json(simplifyVector = TRUE)
+}
