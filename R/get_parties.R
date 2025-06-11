e9157ce042570ddd542b13384c471da9e1c2ee52
MWypych <wypych.mic@gmail.com>
2025-06-11 10:14:40 +0200

    add get_parties

---
+++ R/get_parties.R
@@ -0,0 +1,18 @@
+# get parties basic data
+
+
+
+get_parties <- function(term = 10) {
+
+  req_url <- paste0("https://api.sejm.gov.pl/sejm/term", term, "/")
+  req <- request(req_url)
+
+  req |>
+    req_url_path_append("clubs") |>
+    req_user_agent("sejm (https://github.com/mic-wypych/sejm)") |>
+    req_perform() |>
+    resp_body_json(simplifyVector = TRUE)
+
+
+
+}
