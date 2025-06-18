0e4e436385f7d1c6bf68a1e343b1093b018bde85
MWypych <wypych.mic@gmail.com>
2025-06-18 09:24:24 +0200

    start making ui and server logic for the app

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,7 +17,7 @@
 ## Amend DESCRIPTION with dependencies read from package code parsing
 ## install.packages('attachment') # if needed.
 attachment::att_amend_desc()
-
+ 
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module(name = "name_of_module1", with_test = TRUE) # Name of the module
